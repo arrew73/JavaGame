b2ea264de2820de616706d53398814c9b4d446db
Andrew <sg.pelvis@gmail.com>
2020-06-13 17:31:13 +0400

    first commit

---
+++ minesweeper/GameObject.java
@@ -0,0 +1,14 @@
+package com.javarush.games.minesweeper;
+
+public class GameObject {
+    public boolean isMine;
+    public int x;
+    public int y;
+    public int countMineNeighbors;
+
+    GameObject(int x, int y, boolean isMine) {
+        this.x = x;
+        this.y = y;
+        this.isMine = isMine;
+    }
+}
